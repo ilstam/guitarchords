d7b85e9c313416a0cf16bf92ad63f9bc9c18b3eb
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-05 16:21:54 +0300

    create song and singer models

--- chords/models.py
+++ chords/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Singer(models.Model):
+    name = models.CharField(max_length=80)
+    surname = models.CharField(max_length=80)
+
+    def __str__(self):
+        return "{0} {1}".format(self.name, self.surname) if self.name else self.surname
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    singer = models.ForeignKey(Singer)
+    content = models.TextField()
+    #genre = models.ForeignKey(Genre)
+    video = models.URLField(default="")
+    tabs = models.BooleanField('tablatures')
+    #sender = models.ForeignKey(User)
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    mod_date = models.DateTimeField('last modified', auto_now=True)
+    slug = models.SlugField(unique=True, max_length=60)
+
+    def __str__(self):
+        return "{0}: {1}".format(self.singer, self.title)
