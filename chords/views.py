35cd74ad549e0cf1a2eb346dc578fa6451057dea
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-06 01:02:23 +0300

    display the most recently published songs in the index page

--- chords/views.py
+++ chords/views.py
@@ -4,8 +4,8 @@ from .models import Artist, Song
 
 
 def index(request):
-    context = {'hello_msg' : 'This is the index page'}
-    return render(request, 'chords/index.html', context)
+    recent_songs = Song.objects.order_by('-pub_date')[:5]
+    return render(request, 'chords/index.html', {'songs' : recent_songs})
 
 def song(request, song_slug):
     song = get_object_or_404(Song, slug=song_slug)
