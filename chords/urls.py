06c19648415024585c7106405d46007857292118
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-06 00:49:47 +0300

    setup song view, template, urls

--- chords/urls.py
+++ chords/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^song/(?P<song_slug>[\w\-]+)/$', views.song, name='song'),
     url(r'^about/$', views.about, name='about'),
 ]
--- chords/views.py
+++ chords/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Artist, Song
+
+
 def index(request):
     context = {'hello_msg' : 'This is the index page'}
     return render(request, 'chords/index.html', context)
+
+def song(request, song_slug):
+    song = get_object_or_404(Song, slug=song_slug)
+    return render(request, 'chords/song.html', {'song' : song})
 
 def about(request):
     return render(request, 'chords/about.html', {})
