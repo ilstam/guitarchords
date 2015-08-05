38540a203cbb89613776220166384eac9429755b
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-05 13:39:34 +0300

    create first templates and view

--- chords/views.py
+++ chords/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {'hello_msg' : 'This is the index page'}
+    return render(request, 'chords/index.html', context)
