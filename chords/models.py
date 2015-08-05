a8c83f88d401a8b1a813e7a4a01a5fa9e89fe8d9
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-05 22:38:52 +0300

    artists should only have a name, update tests as well

--- chords/models.py
+++ chords/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from .utils import generate_unique_slug, greek_to_english_letters
 
 class Artist(models.Model):
+    # names are stored in the "Surname Name" format
     name = models.CharField(max_length=80)
-    surname = models.CharField(max_length=80)
 
     def __str__(self):
-        return '{0} {1}'.format(self.name, self.surname) if self.name else self.surname
+        return self.name
 
 class Song(models.Model):
     title = models.CharField(max_length=100)
@@ -24,8 +24,7 @@ class Song(models.Model):
     def save(self, slug_max_length=-1, *args, **kwargs):
         if not self.id:
             # generate slug only when creating an object to avoid broken links
-            slug_text = '{0} {1}'.format(self.artist.surname, self.title)
-            slug_text = greek_to_english_letters(slug_text)
+            slug_text = greek_to_english_letters(self.title)
             self.slug = generate_unique_slug(Song, slug_text, slug_max_length)
 
         super(Song, self).save(*args, **kwargs)
