b7a315b97f6665f0377d88ca306b7365c9867f80
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-05 14:20:11 +0300

    namespace url names

--- guitarchords/urls.py
+++ guitarchords/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^chords/', include('chords.urls')),
+    url(r'^chords/', include('chords.urls', namespace='chords')),
 ]
