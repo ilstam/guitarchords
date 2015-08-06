6f13b8e03e02ef00a8f43e309ae025101c51a680
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-06 20:42:23 +0300

    seperate registration date and publication date

--- chords/models.py
+++ chords/models.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from django.db import models
 
 from .utils import generate_unique_slug, greek_to_english_letters
+
 
 class Artist(models.Model):
     # names are stored in the "Surname Name" format
@@ -18,17 +21,26 @@ class Artist(models.Model):
     def __str__(self):
         return self.name
 
+
 class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.ForeignKey(Artist)
     content = models.TextField(default='')
     #genre = models.ForeignKey(Genre)
-    video = models.URLField(default='')
+    video = models.URLField(blank=True)
     tabs = models.BooleanField('tablatures', default=False)
     #sender = models.ForeignKey(User)
-    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    published = models.BooleanField(default=False)
+    # when the song initially submitted by a user for approval
+    reg_date = models.DateTimeField('date registered', auto_now_add=True)
+    # when the song actually published after approval
+    pub_date = models.DateTimeField('date published', null=True, blank=True)
     mod_date = models.DateTimeField('last modified', auto_now=True)
     slug = models.SlugField(unique=True, max_length=60)
+
+    def __init__(self, *args, **kwargs):
+        super(Song, self).__init__(*args, **kwargs)
+        self.old_published = self.published
 
     def save(self, slug_max_length=-1, *args, **kwargs):
         if not self.id:
@@ -37,6 +49,11 @@ class Song(models.Model):
             self.slug = generate_unique_slug(Song, slug_text, slug_max_length)
 
         self.content = self.content.strip('\n')
+
+        if self.old_published != self.published and self.published:
+            self.pub_date = datetime.now()
+        self.old_published = self.published
+
         super(Song, self).save(*args, **kwargs)
 
     def full_title(self):
