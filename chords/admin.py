870cb0d8e4e734e19427e55f8407ccfba3152c6a
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-06 21:03:10 +0300

    automatically generate slugs in django admin interface

--- chords/admin.py
+++ chords/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import Artist, Song
 
 
-admin.site.register(Artist)
-admin.site.register(Song)
+class ArtistAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+class SongAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+
+
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Song, SongAdmin)
