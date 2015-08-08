8aefec753bba1416a8ddc95770681c8a37d2a81e
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-08 18:51:07 +0300

    improve form representation

--- chords/forms.py
+++ chords/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, Textarea
+from django.forms import ModelForm, Textarea, TextInput
 
 from .models import Song
 
@@ -7,6 +7,30 @@ class AddSongForm(ModelForm):
     class Meta:
         model = Song
         fields = ['title', 'artist', 'genre', 'video', 'tabs', 'content']
+
         widgets = {
+            'artist' : TextInput,
             'content' : Textarea(attrs={'cols': 70, 'rows': 35}),
+        }
+
+        labels = {
+            'title' : 'Title',
+            'artist' : 'Artist',
+            'genre' : 'Genre',
+            'video' : 'Video',
+            'tabs' : 'Tabs',
+            'content' : '',
+        }
+
+        help_texts = {
+            'title' : 'required',
+            'artist' : 'Surname Name format, required',
+            'video' : 'Youtube etc.',
+            'tabs' : 'does this version contain tabs?',
+        }
+
+        error_messages = {
+            'content' : {
+                'required' : "Remember to include the song!"
+            },
         }
