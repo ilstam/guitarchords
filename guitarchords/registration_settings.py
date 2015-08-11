7beb24a3465a33d64b4bced93b84fe47b7fdb03e
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-11 19:10:31 +0300

    apply more registration settings

--- guitarchords/registration_settings.py
+++ guitarchords/registration_settings.py
@@ -1,3 +1,8 @@
 # settings specific to the django-registration-redux app
 
-ACCOUNT_ACTIVATION_DAYS = 7 # one-week activation window
+REGISTRATION_OPEN = True         # users can register
+ACCOUNT_ACTIVATION_DAYS = 7      # one-week activation window
+LOGIN_REDIRECT_URL = '/chords/'  # redirect here after a successful log in
+LOGIN_URL = '/accounts/login/'   # the page users are directed to if they are not logged in,
+                                 # and are trying to access pages requiring authentication
+REGISTRATION_EMAIL_HTML = False  # disable html emails (just use the txt version)
