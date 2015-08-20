1c9b6fb501c9afe28404f80f79c0a22bfb98887c
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-20 19:27:03 +0300

    exclude test folder from testing

--- scripts/test_coverage.sh
+++ scripts/test_coverage.sh
@@ -2,5 +2,5 @@
 # run tests and display coverage report
 
 cd ..
-coverage run --source='.' --omit=guitarchords/*,populate_chords.py,manage.py,chords/tests.py,chords/migrations/* manage.py test chords
+coverage run --source='chords' --omit=chords/tests/*,chords/migrations/* manage.py test chords
 coverage report
