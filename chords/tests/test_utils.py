0d75b46fcb3c6f1822a57a48526776b088707b3f
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-08-28 02:12:20 +0300

    strip_whitespace_lines should not strip no whitespace lines

--- chords/tests/test_utils.py
+++ chords/tests/test_utils.py
@@ -4,7 +4,7 @@ from chords import utils
 
 
 class TestUtils(SimpleTestCase):
-    def test_strip_whitespace_lines(self):
+    def test_strip_whitespace_lines_strips_whitespace(self):
         """
         The strip_empty_lines() function should remove all empty lines from
         end and begging and any adjacent whitespace lines inside.
@@ -12,3 +12,11 @@ class TestUtils(SimpleTestCase):
         s1 = '  \t\n\n\t \tsome identation here\n\n\n\nlorem ipsum\n\n'
         s2 =         '\t \tsome identation here\n\nlorem ipsum'
         self.assertEqual(utils.strip_whitespace_lines(s1), s2)
+
+    def test_strip_whitespace_lines_not_strip_no_whitespace_lines(self):
+        """
+        The strip_empty_lines() function should not remove any other adjacent
+        lines except whitespace lines.
+        """
+        s = 'lorem ipsum\nlorem ipsum'
+        self.assertEqual(utils.strip_whitespace_lines(s), s)
