ef2265ad22dde2de5d779b7507ea787ad063dc70
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-09-16 01:02:04 +0300

    add publish and unpublish admin actions

--- chords/admin.py
+++ chords/admin.py
@@ -6,7 +6,6 @@ from .models import Artist, Song
 admin.AdminSite.site_title = 'Chords administration'
 admin.AdminSite.site_header = 'Chords Administration'
 
-
 class ArtistAdmin(admin.ModelAdmin):
     exclude = ['slug']
 
@@ -20,10 +19,17 @@ class SongAdmin(admin.ModelAdmin):
     ]
 
     list_display = ['full_title', 'reg_date', 'pub_date', 'published']
-
     list_filter = ['pub_date', 'reg_date', 'genre', 'tabs']
-
     search_fields = ['sender__username', 'artist__name']
+    actions = ['publish_songs', 'unpublish_songs']
+
+    def publish_songs(self, request, queryset):
+        queryset.update(published=True)
+    publish_songs.short_description = 'Publish all selected songs'
+
+    def unpublish_songs(self, request, queryset):
+        queryset.update(published=False)
+    unpublish_songs.short_description = 'Unpublish all selected songs'
 
 
 admin.site.register(Artist, ArtistAdmin)
