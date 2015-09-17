93f62790f886247375752ed7db143c137f6672b9
Ilias Stamatis <stamatis.iliass@gmail.com>
2015-09-17 18:53:34 +0300

    avoid repeating song url path

--- chords/urls.py
+++ chords/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from . import views
 
 
+song_path = r'^song/(?P<song_slug>[\w\-]+)'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^song/(?P<song_slug>[\w\-]+)/$', views.song, name='song'),
-    url(r'^song/(?P<song_slug>[\w\-]+)/.json/$', views.song_json, name='song_json'),
+    url(song_path + '/$', views.song, name='song'),
+    url(song_path + '/.json/$', views.song_json, name='song_json'),
     url(r'^artist/(?P<artist_slug>[\w\-]+)/$', views.artist, name='artist'),
     url(r'^add_song/$', views.add_song, name='add_song'),
     url(r'^verify_song/$', views.verify_song, name='verify_song'),
@@ -15,6 +17,6 @@ urlpatterns = [
     url(r'^popular/$', views.popular, name='popular'),
     url(r'^search/$', views.search, name='search'),
     url(r'^bookmarks/$', views.bookmarks, name='bookmarks'),
-    url(r'^song/(?P<song_slug>[\w\-]+)/add_bookmark/$', views.add_bookmark, name='add_bookmark'),
-    url(r'^song/(?P<song_slug>[\w\-]+)/remove_bookmark/$', views.remove_bookmark, name='remove_bookmark'),
+    url(song_path + '/add_bookmark/$', views.add_bookmark, name='add_bookmark'),
+    url(song_path + '/remove_bookmark/$', views.remove_bookmark, name='remove_bookmark'),
 ]
